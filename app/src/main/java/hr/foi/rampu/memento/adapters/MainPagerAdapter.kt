a48ec5d442d82a9e3e89da1cf68ae58a5e1f5017
Mislav Matijević <mislav.mim@gmail.com>
2022-10-14 23:41:35 +0200

    Implemented MainPagerAdapter. Also created a special type "FragmentItem" for containing all Fragment information.

--- app/src/main/java/hr/foi/rampu/memento/adapters/MainPagerAdapter.kt
+++ app/src/main/java/hr/foi/rampu/memento/adapters/MainPagerAdapter.kt
@@ -4,14 +4,22 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import kotlin.reflect.KClass
 
 class MainPagerAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
     FragmentStateAdapter(fragmentManager, lifecycle) {
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+
+    data class FragmentItem(val titleRes: Int, val iconRes: Int, val fragmentClass: KClass<*>)
+
+    private val fragmentItems = ArrayList<FragmentItem>()
+
+    fun addFragment(fragment: FragmentItem) {
+        fragmentItems.add(fragment)
     }
 
+    override fun getItemCount(): Int = fragmentItems.size
+
     override fun createFragment(position: Int): Fragment {
-        TODO("Not yet implemented")
+        return fragmentItems[position].fragmentClass.java.newInstance() as Fragment
     }
 }
