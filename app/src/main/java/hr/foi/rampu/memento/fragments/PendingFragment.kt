3355ce7c5006c42962049ec79268b05d20d81005
Mislav Matijević <mislav.mim@gmail.com>
2022-10-17 17:47:25 +0200

    RecyclerView referenced in code. For future use. Added variable for a list of mock tasks, also for future use.

--- app/src/main/java/hr/foi/rampu/memento/fragments/PendingFragment.kt
+++ app/src/main/java/hr/foi/rampu/memento/fragments/PendingFragment.kt
@@ -5,14 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import hr.foi.rampu.memento.R
+import hr.foi.rampu.memento.helpers.MockDataLoader
 
 class PendingFragment : Fragment() {
+    private val mockTasks = MockDataLoader.getDemoData()
+    private lateinit var recyclerView: RecyclerView
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         return inflater.inflate(R.layout.fragment_pending, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        recyclerView = view.findViewById(R.id.rv_pending_tasks)
     }
 }
