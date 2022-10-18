e2e293432cb7e67dd2f7611aa7c3a88fe053b969
Mislav Matijević <mislav.mim@gmail.com>
2022-10-18 13:08:02 +0200

    Prepared empty TasksAdapter.  It has its own TaskViewHolder ready.

---
+++ app/src/main/java/hr/foi/rampu/memento/adapters/TasksAdapter.kt
@@ -0,0 +1,23 @@
+package hr.foi.rampu.memento.adapters
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class TasksAdapter : RecyclerView.Adapter<TasksAdapter.TaskViewHolder>() {
+    inner class TaskViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
