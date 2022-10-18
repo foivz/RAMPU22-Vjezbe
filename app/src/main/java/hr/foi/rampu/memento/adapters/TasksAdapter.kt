347a01331fe7511390175c67c098082b71c35d14
Mislav Matijević <mislav.mim@gmail.com>
2022-10-18 22:56:36 +0200

    Implemented TaskViewHolder.

--- app/src/main/java/hr/foi/rampu/memento/adapters/TasksAdapter.kt
+++ app/src/main/java/hr/foi/rampu/memento/adapters/TasksAdapter.kt
@@ -2,11 +2,29 @@ package hr.foi.rampu.memento.adapters
 
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import hr.foi.rampu.memento.R
+import hr.foi.rampu.memento.entities.Task
+import java.text.SimpleDateFormat
+import java.util.*
 
 class TasksAdapter : RecyclerView.Adapter<TasksAdapter.TaskViewHolder>() {
     inner class TaskViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val sdf: SimpleDateFormat = SimpleDateFormat("dd.MM.yyyy. HH:mm", Locale.ENGLISH)
 
+        private val taskName: TextView
+        private val taskDueDate: TextView
+
+        init {
+            taskName = view.findViewById(R.id.tv_task_name)
+            taskDueDate = view.findViewById(R.id.tv_task_due_date)
+        }
+
+        fun bind(task: Task) {
+            taskName.text = task.name
+            taskDueDate.text = sdf.format(task.dueDate)
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
