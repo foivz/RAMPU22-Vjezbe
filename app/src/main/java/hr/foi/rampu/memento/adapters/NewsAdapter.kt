5da6210d608fa6d7e8a920e5fddb07a119b868ab
Mislav Matijević <mislav.mim@gmail.com>
2022-11-06 21:03:41 +0100

    Added NewsAdapter. Inside of it is its ViewHolder. Notice how, unlike the TaskViewHolder, this nested NewsViewHolder class is not marked with "inner" and therefore has no access to the properties of NewsAdapter (because there's no need for that).

---
+++ app/src/main/java/hr/foi/rampu/memento/adapters/NewsAdapter.kt
@@ -0,0 +1,44 @@
+package hr.foi.rampu.memento.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import hr.foi.rampu.memento.R
+import hr.foi.rampu.memento.ws.WsNewsResult
+
+class NewsAdapter(private val newsList: List<WsNewsResult>) : RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
+    class NewsViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        private var title: TextView
+        private var contentText: TextView
+        private var dateTime: TextView
+        private var imageView: ImageView
+
+        init {
+            title = view.findViewById(R.id.tv_news_list_item_title)
+            contentText = view.findViewById(R.id.tv_news_list_item_text)
+            dateTime = view.findViewById(R.id.tv_news_list_item_date)
+            imageView = view.findViewById(R.id.iv_news_list_item_image)
+        }
+
+        fun bind(newsItem: WsNewsResult) {
+            title.text = newsItem.title
+            contentText.text = newsItem.text
+            dateTime.text = newsItem.date
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
+        val newsView = LayoutInflater.from(parent.context).inflate(R.layout.news_list_item, parent, false)
+        return NewsViewHolder(newsView)
+    }
+
+    override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
+        holder.bind(newsList[position])
+    }
+
+    override fun getItemCount(): Int = newsList.size
+}
