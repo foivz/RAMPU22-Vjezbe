4b438a2fbc47c8d730a8475ba17a1b4b8f00261d
Mislav Matijević <mislav.mim@gmail.com>
2022-11-06 23:29:53 +0100

    Displaying images alongside news items. Using newly added Picasso dependency to download a photo and display it next to the news item.

--- app/src/main/java/hr/foi/rampu/memento/adapters/NewsAdapter.kt
+++ app/src/main/java/hr/foi/rampu/memento/adapters/NewsAdapter.kt
@@ -6,7 +6,9 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 import hr.foi.rampu.memento.R
+import hr.foi.rampu.memento.ws.WsNews
 import hr.foi.rampu.memento.ws.WsNewsResult
 
 class NewsAdapter(private val newsList: List<WsNewsResult>) : RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
@@ -28,6 +30,7 @@ class NewsAdapter(private val newsList: List<WsNewsResult>) : RecyclerView.Adapt
             title.text = newsItem.title
             contentText.text = newsItem.text
             dateTime.text = newsItem.date
+            Picasso.get().load(WsNews.BASE_URL + newsItem.imagePath).into(imageView)
         }
     }
 
