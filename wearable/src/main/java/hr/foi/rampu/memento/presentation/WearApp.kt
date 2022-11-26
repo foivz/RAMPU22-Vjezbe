2f61f06bbf5f61b7561f72981d8445934877725a
Mislav Matijević <mislav.mim@gmail.com>
2022-11-26 13:03:49 +0100

    [Wearable] Moved main Compose function to its own file.

---
+++ wearable/src/main/java/hr/foi/rampu/memento/presentation/WearApp.kt
@@ -0,0 +1,9 @@
+package hr.foi.rampu.memento.presentation
+
+import androidx.compose.runtime.Composable
+import hr.foi.rampu.memento.presentation.models.Task
+
+@Composable
+fun WearApp(tasks: List<Task>) {
+
+}
