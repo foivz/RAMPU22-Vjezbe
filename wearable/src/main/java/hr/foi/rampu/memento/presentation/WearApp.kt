e7c7fd419e314f930be30d267f4c490abd4f68b9
Mislav Matijević <mislav.mim@gmail.com>
2022-11-26 13:27:17 +0100

    [Wearable] Improved design of wearable tasks' list.

--- wearable/src/main/java/hr/foi/rampu/memento/presentation/WearApp.kt
+++ wearable/src/main/java/hr/foi/rampu/memento/presentation/WearApp.kt
@@ -1,29 +1,64 @@
 package hr.foi.rampu.memento.presentation
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
-import androidx.wear.compose.material.Card
-import androidx.wear.compose.material.ScalingLazyColumn
-import androidx.wear.compose.material.Text
-import androidx.wear.compose.material.items
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.wear.compose.material.*
+import hr.foi.rampu.memento.R
 import hr.foi.rampu.memento.presentation.models.Task
+import hr.foi.rampu.memento.presentation.theme.MementoBlue
+import hr.foi.rampu.memento.presentation.theme.MementoDarkerBlue
+import hr.foi.rampu.memento.presentation.theme.MementoTheme
 
 @Composable
 fun WearApp(tasks: List<Task>) {
-    ScalingLazyColumn {
-        items(tasks) { task ->
-            TaskCard(task)
+
+    val listState = rememberScalingLazyListState()
+
+    MementoTheme {
+        Scaffold(positionIndicator = { PositionIndicator(scalingLazyListState = listState) },
+            vignette = { Vignette(vignettePosition = VignettePosition.TopAndBottom) }) {
+            if (tasks.isEmpty()) {
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight()
+                )
+                Column(
+                    modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        stringResource(R.string.msg_wait_tasks_syncing),
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Center
+                    )
+                }
+            } else {
+                ScalingLazyColumn {
+                    items(tasks) { task ->
+                        TaskTexts(task)
+                    }
+                }
+            }
         }
     }
 }
 
 
 @Composable
-fun TaskCard(task: Task) {
+fun TaskTexts(
+    task: Task
+) {
     Card(onClick = {}) {
         Column {
-            Text(task.name)
-            Text(task.categoryName)
+            Text(
+                task.name, style = MaterialTheme.typography.title3, color = MementoBlue
+            )
+            Text(
+                task.categoryName, style = MaterialTheme.typography.body2, color = MementoDarkerBlue
+            )
         }
     }
 }
