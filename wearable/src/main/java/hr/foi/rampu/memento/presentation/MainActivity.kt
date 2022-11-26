35ae2794f5112a8a057a4c66a04ad32ebad36690
Mislav Matijević <mislav.mim@gmail.com>
2022-11-26 13:35:04 +0100

    [Wearable] Implemented DataClient onDataChanged listener method. Wearable app displays tasks received from the cloud.

--- wearable/src/main/java/hr/foi/rampu/memento/presentation/MainActivity.kt
+++ wearable/src/main/java/hr/foi/rampu/memento/presentation/MainActivity.kt
@@ -10,9 +10,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.mutableStateListOf
-import com.google.android.gms.wearable.DataClient
-import com.google.android.gms.wearable.DataEventBuffer
-import com.google.android.gms.wearable.Wearable
+import com.google.android.gms.wearable.*
 import hr.foi.rampu.memento.presentation.models.Task
 
 class MainActivity : ComponentActivity(), DataClient.OnDataChangedListener {
@@ -38,6 +36,24 @@ class MainActivity : ComponentActivity(), DataClient.OnDataChangedListener {
     }
 
     override fun onDataChanged(dataEvents: DataEventBuffer) {
-        TODO("Not yet implemented")
+        dataEvents.forEach { dataEvent ->
+            if (dataEvent.type == DataEvent.TYPE_CHANGED) {
+                DataMapItem.fromDataItem(dataEvent.dataItem).apply {
+                    val tasksCount = dataMap.getInt("tasks_count")
+
+                    for (i in 0 until tasksCount) {
+                        tasks.add(
+                            Task(
+                                dataMap.getInt("task_id_$i"),
+                                dataMap.getString("task_name_$i")!!,
+                                dataMap.getString("task_category_name_$i")!!
+                            )
+                        )
+                    }
+
+                    dataClient.deleteDataItems(uri)
+                }
+            }
+        }
     }
 }
