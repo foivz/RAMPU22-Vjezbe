fd2d9f3b8c1edac2ea53014a944303e540aec40d
Mislav Matijević <mislav.mim@gmail.com>
2022-11-29 18:06:59 +0100

    Showing MementoSettingsFragment fragment in PreferencesActivity.

--- app/src/main/java/hr/foi/rampu/memento/PreferencesActivity.kt
+++ app/src/main/java/hr/foi/rampu/memento/PreferencesActivity.kt
@@ -2,10 +2,16 @@ package hr.foi.rampu.memento
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import hr.foi.rampu.memento.fragments.MementoSettingsFragment
 
 class PreferencesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_preferences)
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container_settings, MementoSettingsFragment())
+            .commit()
     }
 }
