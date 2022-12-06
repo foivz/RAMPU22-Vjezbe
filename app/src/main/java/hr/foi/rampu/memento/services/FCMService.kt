a04e14c28751dd24b99b7b3c31b45a783f68dcf5
Mislav Matijević <mislav.mim@gmail.com>
2022-12-06 13:30:37 +0100

    FCM service receives logs messages.

--- app/src/main/java/hr/foi/rampu/memento/services/FCMService.kt
+++ app/src/main/java/hr/foi/rampu/memento/services/FCMService.kt
@@ -1,10 +1,16 @@
 package hr.foi.rampu.memento.services
 
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
 class FCMService : FirebaseMessagingService() {
+    override fun onCreate() {
+        FirebaseMessaging.getInstance().subscribeToTopic("news")
+    }
+
     override fun onMessageReceived(message: RemoteMessage) {
-        super.onMessageReceived(message)
+        Log.i("MEMENTO_FCM_MSG", "Message data payload: ${message.data}")
     }
 }
