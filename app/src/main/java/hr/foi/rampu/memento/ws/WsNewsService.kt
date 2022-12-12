0b53faa3a4fd55a8d0b8cc7e534a78cd6275ab30
Mislav Matijević <mislav.mim@gmail.com>
2022-12-12 10:18:59 +0100

    Added new type of WsNewsService request. Now, only one news item can be fetched by its title.

--- app/src/main/java/hr/foi/rampu/memento/ws/WsNewsService.kt
+++ app/src/main/java/hr/foi/rampu/memento/ws/WsNewsService.kt
@@ -2,8 +2,12 @@ package hr.foi.rampu.memento.ws
 
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface WsNewsService {
     @GET("news.php")
     fun getNews(): Call<WsNewsResponse>
+
+    @GET("news.php")
+    fun getNews(@Query("title") newsTitle: String): Call<WsNewsResult>
 }
