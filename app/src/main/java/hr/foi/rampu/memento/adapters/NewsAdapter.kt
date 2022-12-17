8f152ed5f209b0c69d4e859b0d723cc94daecd9f
Mislav Matijević <mislav.mim@gmail.com>
2022-12-17 21:37:59 +0100

    Enabled user to view a news item. Long tap on news item in news fragment displays NewsActivity.

--- app/src/main/java/hr/foi/rampu/memento/adapters/NewsAdapter.kt
+++ app/src/main/java/hr/foi/rampu/memento/adapters/NewsAdapter.kt
@@ -1,18 +1,21 @@
 package hr.foi.rampu.memento.adapters
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
+import hr.foi.rampu.memento.NewsActivity
 import hr.foi.rampu.memento.R
 import hr.foi.rampu.memento.ws.WsNews
 import hr.foi.rampu.memento.ws.WsNewsResult
 
 class NewsAdapter(private val newsList: List<WsNewsResult>) : RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
-    class NewsViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    class NewsViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
 
         private var title: TextView
         private var contentText: TextView
@@ -31,6 +34,15 @@ class NewsAdapter(private val newsList: List<WsNewsResult>) : RecyclerView.Adapt
             contentText.text = newsItem.text
             dateTime.text = newsItem.date
             Picasso.get().load(WsNews.BASE_URL + newsItem.imagePath).into(imageView)
+
+            view.setOnLongClickListener {
+                val intent = Intent(view.context, NewsActivity::class.java).apply {
+                    putExtra("news_name", title.text)
+                }
+                ContextCompat.startActivity(view.context, intent, null)
+
+                return@setOnLongClickListener true
+            }
         }
     }
 
